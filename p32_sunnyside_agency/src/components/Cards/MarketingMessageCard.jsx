88c52964c2b9053227386cec89064d581f258965
mintdexdev <dexdevpersona@gmail.com>
2025-07-09 21:27:57 +0530

    add basic animation

--- p32_sunnyside_agency/src/components/Cards/MarketingMessageCard.jsx
+++ p32_sunnyside_agency/src/components/Cards/MarketingMessageCard.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { motion } from 'motion/react'
 import { LearnMoreCTA } from '../index'
 
 function MarketingMessageCard({ imageSrc, alt, title, description, flip = false }) {
+
+  const fadeInUp = {
+    initial: { opacity: 0, y: 20 },
+    whileInView: { opacity: 1, y: 0 },
+    transition: { duration: 0.6 },
+  };
+
+  const fadeInUpDelayed = {
+    ...fadeInUp,
+    transition: { ...fadeInUp.transition, delay: 0.2 },
+  };
+
   return (
     <div className={`w-full flex flex-col sm:flex-row ${flip ? "sm:flex-row-reverse" : ""}`}>
       <img
@@ -11,9 +24,21 @@ function MarketingMessageCard({ imageSrc, alt, title, description, flip = false 
       <div
         className='w-full px-4 pt-4 pb-12 flex flex-col
         sm:w-[50%] sm:p-8 sm:justify-center lg:p-20'>
-        <h2 className='mb-6 text-4xl font-bold'>{title}</h2>
-        <p className='text-neutral-600 font-semibold'>{description}</p>
-        <LearnMoreCTA />
+
+        <motion.h2 className='mb-6 text-4xl font-bold'
+          {...fadeInUp}
+        >
+          {title}
+        </motion.h2>
+
+        <motion.p
+          className='text-neutral-600 font-semibold'
+          {...fadeInUpDelayed}
+        >
+          {description}
+        </motion.p>
+        <LearnMoreCTA
+        />
       </div>
     </div>
   )
--- p32_sunnyside_agency/src/components/Header.jsx
+++ p32_sunnyside_agency/src/components/Header.jsx
@@ -1,18 +1,69 @@
-import React, { useEffect, useState } from 'react'
+import { motion, useAnimation } from "motion/react"
+
 import { Logo } from './'
+import { useEffect } from "react";
 
 function Header({ device }) {
-
-
 
   const navList = [
     { name: "About", },
     { name: "Services", },
     { name: "Projects", },
   ]
+
+  const contactBtnEffect = {
+    initial: {
+      backgroundColor: 'hsl(0, 0%, 100%)',
+    },
+
+    whileHover: {
+      backgroundColor: 'hsla(0, 0%, 100%, 0.5)',
+    },
+    whileTap: {
+      scale: 0.95,
+    },
+    transition: {
+      backgroundColor: { duration: 0.1 },
+      scale: { duration: 0.1 }
+    }
+  }
+
+  const arrowFlickControls = useAnimation();
+  useEffect(() => {
+    let loopCount = 0;
+
+    const bounce = async () => {
+      while (true) {
+        await arrowFlickControls.start({
+          y: [0, 16, 0],
+          transition: {
+            duration: 5,
+            ease: 'easeInOut',
+            times: [0, 0.6, 1],
+          },
+        });
+        loopCount++;
+
+        if (loopCount === 5) {
+          await arrowFlickControls.start({
+            y: [0, 8, 0, 8, 0],
+            transition: {
+              duration: 0.6,
+              ease: 'easeInOut',
+            },
+          });
+          loopCount = 0;
+        }
+      }
+
+    };
+
+    bounce();
+  }, [arrowFlickControls]);
+
   return (
     <header className='relative'>
-      <nav className='absolute w-full px-8 py-10 flex justify-between items-center lg:px-12 lg:pt-10'>
+      <nav className='absolute w-full px-8 py-6 flex justify-between items-center lg:px-12 lg:pt-6'>
         <div>
           <Logo />
         </div>
@@ -21,7 +72,7 @@ function Header({ device }) {
             src="/images/icon-hamburger.svg"
             alt="menu btn"
           />
-          : <ul className='flex gap-8'>
+          : <ul className='flex gap-8 items-center'>
             {navList.map(item => (
               <li
                 key={item.name}>
@@ -32,12 +83,29 @@ function Header({ device }) {
             ))}
 
             <li>
-              <a
-                className='px-6 py-3 rounded-full text-lg font-semibold bg-neutral-100 text-neutral-900 hover:bg-neutral-100/40 hover:text-neutral-100'
+              <motion.a
                 href="#"
+                className="inline-block px-6 py-3 rounded-full text-lg font-semibold bg-neutral-100 text-neutral-900
+                will-change-transform"
+                {...contactBtnEffect}
+
+              // initial={{
+              //   backgroundColor: 'hsl(0, 0%, 100%)',
+              // }}
+              // whileHover={{
+              //   backgroundColor: 'hsla(0, 0%, 100%, 0.5)',
+              // }}
+              // whileTap={{
+              //   scale: 0.95,
+              // }}
+              // transition={{
+              //   backgroundColor: { duration: 0.1 },
+              //   scale: { duration: 0.1 },
+              // }}
+
               >
                 Contact
-              </a>
+              </motion.a>
             </li>
           </ul>
         }
@@ -48,12 +116,14 @@ function Header({ device }) {
         absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
           <h1 className='m-4 text-5xl sm:text-5xl lg:text-7xl uppercase tracking-widest text-white'
           >
-            We are creatives
+            <div>We are</div>
+            <div>Creatives</div>
           </h1>
-          <img
-            className='h-[80px] mx-auto my-10 sm:h-[]       lg:my-20 md:h-fit'
+          <motion.img
+            className='h-[80px] mx-auto my-10  lg:my-16 md:h-fit'
             src="/images/icon-arrow-down.svg"
             alt="down arrow"
+            animate={arrowFlickControls}
           />
         </div>
         <img
