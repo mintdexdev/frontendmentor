a140063a0765481ee4f0c1c4e0408e593752f059
mintdexdev <dexdevpersona@gmail.com>
2025-07-18 23:02:58 +0530

    project setup

---
+++ p36_space_tourism/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      00 Home
+      01 Destination
+      02 Crew
+      03 Technology
+
+      So, you want to travel to
+      Space
+      Let’s face it; if you want to go to space, you might as well genuinely go to
+      outer space and not hover kind of on the edge of it. Well sit back, and relax
+      because we’ll give you a truly out of this world experience!
+
+      Explore
+      
+    </div>
+  )
+}
+
+export default App
